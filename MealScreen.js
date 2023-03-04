4ecc042f4a629b569c36a8a547b1b960c5900029
euije <exitfill1234@naver.com>
2023-03-05 02:30:19 +0900

    feat: Implement page of meal

--- MealScreen.js
+++ MealScreen.js
@@ -1,48 +1,86 @@
-import { useState, useEffect } from 'react'
-import { View, Text } from 'react-native'
-import { parse } from 'node-html-parser'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Button
+} from 'react-native';
+import * as Font from 'expo-font';
 
-// await 을 function 안에서 사용하지 못하는 이유는?
-// 저렇게 promise chaining 하지 않고 해결할 수 있는 방법이 있을 것 같은데.
+import { parse } from 'node-html-parser';
 
-// const k = await fetch('http://www.cbhs.kr/meal', {
-//   method: 'GET'
-// })
+import { Card } from './components/card.js';
 
-export default function MealScreen() {
-  const [meal, setMeal] = useState({})
+const MealScreen = () => {
+  const [meals, setMeals] = useState([]);
+  let customFonts = {
+    'SpoqaHanSansNeo-Bold': require('./assets/fonts/SpoqaHanSansNeo-Bold.otf'),
+    'SpoqaHanSansNeo-Light': require('./assets/fonts/SpoqaHanSansNeo-Light.otf'),
+    'SpoqaHanSansNeo-Medium': require('./assets/fonts/SpoqaHanSansNeo-Medium.otf'),
+    'SpoqaHanSansNeo-Regular': require('./assets/fonts/SpoqaHanSansNeo-Regular.otf'),
+    'SpoqaHanSansNeo-Thin': require('./assets/fonts/SpoqaHanSansNeo-Thin.otf')
+  };
 
-  useEffect(() => {
-    // fetch('http://www.cbhs.kr/meal', {
-    //   method: 'GET'
-    // }).then((k) => k.text().then((text) => console.log(text)))
-
-    fetch('http://www.cbhs.kr/meal', {
+  const fetchMeal = async () => {
+    let result = [];
+    const k = await fetch('http://www.cbhs.kr/meal', {
       method: 'GET'
-    }).then((k) => {
-      k.text().then((text) => {
-        // console.log(text)
-        const root = parse(text)
-        const fplans = root.querySelectorAll('.fplan_plan')
-        const fplansParsed = fplans.map((fplan) => Array.from(fplan.querySelectorAll('p')))
-        const dateRegex = /[^0-9]/g
-        const unescapeRegex = /\n|\t/g
-        let tmeal = {}
-        fplansParsed.forEach((fplan) => {
-          const date = fplan[0].innerText.replace(dateRegex, '')
-          const brf = fplan[1].innerText.replace(unescapeRegex, '')
-          const lun = fplan[2].innerText.replace(unescapeRegex, '')
-          const din = fplan[3].innerText.replace(unescapeRegex, '')
-          tmeal[date] = { brf, lun, din }
-        })
-        console.log(tmeal)
-      })
+    });
+
+    const text = await k.text();
+    const root = parse(text);
+    const fplans = root.querySelectorAll('.fplan_plan');
+    const fplansParsed = fplans.map((fplan) => Array.from(fplan.querySelectorAll('p')));
+
+    const dateRegex = /[^0-9]/g;
+    const unescapeRegex = /\n|\t/g;
+    fplansParsed.forEach((fplan) => {
+      const date = fplan[0].innerText.replace(dateRegex, '');
+      const day = fplan[0].querySelector('a').innerText;
+      const brf = fplan[1].innerText.replace(unescapeRegex, '').split(',');
+      const lun = fplan[2].innerText.replace(unescapeRegex, '').split(',');
+      const din = fplan[3].innerText.replace(unescapeRegex, '').split(',');
+
+      result.push({ day, brf, lun, din });
     })
+    setMeals(result);
+
+    console.log(JSON.stringify(meals, null, 4));
+  }
+  const loadFont = async () => {await Font.loadAsync(customFonts)};
+  useEffect(() => {
+    fetchMeal();
+    loadFont();
   }, [])
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>asdf</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {
+          meals.length === 0
+            ? <React.Fragment />
+            : meals.map((meal, idx, arr) => <Card key={idx} meal={meal}></Card>)
+        }
+      </ScrollView>
+    </SafeAreaView>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 12,
+    paddingLeft: 24,
+    paddingRight: 24,
+    backgroundColor: 'white',
+  },
+  scrollView: {
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 42,
+  },
+});
+
+export default MealScreen;
