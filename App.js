cb724521cac73a0a15c36e2fa17e72c9b26f6a63
bgb10 <bgb10@naver.com>
2023-03-04 22:49:21 +0900

    로그인 화면 구현
    추후 refactoring 필요!

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import * as React from 'react'
+import { useState, React } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import QRScreen from './QRScreen'
 import MealScreen from './MealScreen'
 import SettingsScreen from './SettingsScreen'
+import LoginScreen from './LoginScreen'
 
 const Tab = createBottomTabNavigator()
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="QR" component={QRScreen} />
-        <Tab.Screen name="Meal" component={MealScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  )
+  const [isSaved, setIsSaved] = useState(false)
+
+  const onLogin = (p) => setIsSaved(p)
+
+  if (isSaved) {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="QR" component={QRScreen} />
+          <Tab.Screen name="Meal" component={MealScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    )
+  } else {
+    return <LoginScreen onLogin={onLogin}></LoginScreen>
+  }
 }
---
+++ LoginScreen.js
@@ -0,0 +1,68 @@
+import { React, useEffect, useState } from 'react'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value)
+    await AsyncStorage.setItem('@storage_Key', jsonValue)
+  } catch (e) {
+    // saving error
+  }
+}
+
+const LoginScreen = (props) => {
+  const [id, setId] = useState('')
+  const [pw, setPw] = useState('')
+
+  const handleLogin = () => {
+    // 개인정보 저장
+    // 로그인 성공할 경우
+    // TODO: 추후에 중복 제거 필요
+    const regex = /makeCode\('(\d+)'\)/
+
+    fetch('http://115.92.96.29:8080/employee/loginProc.jsp', {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      referrer: 'http://115.92.96.29:8080/employee/login.jsp',
+      body: `USER_ID=${id}&USER_PW=${pw}&SAVE_PW=on`,
+      method: 'POST',
+      mode: 'cors'
+    })
+      .then((res) => res.text())
+      .then((text) => {
+        const res = text.match(regex)
+        if (res == null) {
+          console.log('다시 로그인 ㄱ')
+          props.onLogin(false)
+        } else {
+          props.onLogin(true)
+          // 여기서 isSaved 를 true 로 해줘야 하는데 어떻게 할 수 있지?
+          // 부모 state 를 자식이 변경하는게 가능한가? redux 가 필요하려나?
+          // depth 가 깊지 않으므로 굳이 상태관리 라이브러리는 필요 없다. 물론 recoil 사용하면 더 좋긴 함.
+        }
+      })
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>로그인 안되었을 때 초기 화면</Text>
+      <Text>충북학사</Text>
+      <TextInput style={styles.input} value={id} placeholder="학사 번호" onChangeText={setId} />
+      <TextInput style={styles.input} value={pw} placeholder="비밀 번호" onChangeText={setPw} />
+      <Button title="로그인" color="#841584" onPress={handleLogin} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+  }
+})
+
+export default LoginScreen
