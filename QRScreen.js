5cdae8e76c03f8b51fcc4fbb50d3c2a76a797939
bgb10 <bgb10@naver.com>
2023-03-04 16:29:01 +0900

    fetch api 사용해 로그인하고 qr 코드 생성

--- QRScreen.js
+++ QRScreen.js
@@ -1,17 +1,30 @@
 import { View, Text } from 'react-native'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import QRCode from 'react-native-qrcode-svg'
 
 export default function QRScreen() {
-  useEffect(() => {
-    console.log(1)
+  const [code, setCode] = useState('test')
 
-    //TODO: QR 코드 로드하는 로직 넣기
+  useEffect(() => {
+    const regex = /makeCode\('(\d+)'\)/
+
+    fetch('http://115.92.96.29:8080/employee/loginProc.jsp', {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      referrer: 'http://115.92.96.29:8080/employee/login.jsp',
+      body: 'USER_ID=202051&USER_PW=001028&SAVE_PW=on',
+      method: 'POST',
+      mode: 'cors'
+    })
+      .then((res) => res.text())
+      .then((text) => text.match(regex))
+      .then((e) => setCode(e[1]))
   }, [])
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       {/* <Text>QR</Text> */}
-      <QRCode value="http://awesome.link.qr" />
+      <QRCode value={`${code}`} />
     </View>
   )
 }
