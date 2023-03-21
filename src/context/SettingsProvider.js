f8629bc43ddd7b8ad903b68b2044ad7c10128820
bgb10 <bgb10@naver.com>
2023-03-21 16:55:41 +0900

    QR 화면 이동 설정 저장 기능 추가

--- src/context/SettingsProvider.js
+++ src/context/SettingsProvider.js
@@ -1,14 +1,15 @@
 import { View, Text } from 'react-native'
-import { createContext, useContext, useMemo, useRef, useState } from 'react'
+import { createContext, useContext, useMemo, useRef, useState, useEffect } from 'react'
+import { AUTO_QR_SCREEN_MOVE_KEY } from '../data/constants'
+import * as SecureStore from 'expo-secure-store'
 
 export const SettingsContext = createContext()
 
 // 그냥 예를 들어서 세팅할 값이 4개라고 치자. 그럼 어떻게 전달하지?
 // 백그라운드에서 돌아올 때 QR 로 화면 자동 이동, (추후 푸쉬알림 정도?)
 // 일단 화면 자동이동만 고려하고, 나머지는 여쭤보자.
-
 const SettingsProvider = ({ children }) => {
-  const [settings, setSettings] = useState(true)
+  const [settings, setSettings] = useState(false)
   const actions = useMemo(
     () => ({
       toggle() {
@@ -17,6 +18,19 @@ const SettingsProvider = ({ children }) => {
     }),
     []
   )
+
+  const getSettingsFromStorage = async () => {
+    const storedSettings = await SecureStore.getItemAsync(AUTO_QR_SCREEN_MOVE_KEY)
+    setSettings(storedSettings === 'true' ? true : false)
+  }
+
+  useEffect(() => {
+    getSettingsFromStorage()
+  }, [])
+
+  useEffect(() => {
+    SecureStore.setItemAsync(AUTO_QR_SCREEN_MOVE_KEY, `${settings}`)
+  }, [settings])
 
   return <SettingsContext.Provider value={[settings, actions]}>{children}</SettingsContext.Provider>
 }
